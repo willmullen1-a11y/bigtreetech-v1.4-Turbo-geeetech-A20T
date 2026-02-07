5c7b48a8b4cc0399045dc62ac0ceeef43f43cc6a
willmullen1-a11y <will.mullen1@btinternet.com>
2026-02-07 18:31:01 +0000

    Update Configuration.h for complete Marlin configuration for SKR v1.4 Turbo and Geeetech A20T.

--- Configuration.h
+++ Configuration.h
@@ -1,22 +1,30 @@
-// Configuration.h for Marlin firmware
-
+// Configuration.h for Marlin on SKR v1.4 Turbo for Geeetech A20T
 #define MOTHERBOARD BOARD_BTT_SKR_V1_4_TURBO
-#define TEMP_SENSOR_0 1
-#define TEMP_SENSOR_BED 1
 
-// Mechanical Settings
-#define X_BED_SIZE 220
+// Printer dimensions
+#define X_BED_SIZE 250
 #define Y_BED_SIZE 250
-#define Z_BED_HEIGHT 300
+#define Z_DEPTH 250
 
-// Define your endstop settings here
-#define X_MIN_ENDSTOP INVERTING
-#define Y_MIN_ENDSTOP INVERTING
-#define Z_MIN_ENDSTOP INVERTING
+// Thermistor settings
+#define TEMP_SENSOR_0 1  // Hotend NTC 100K
+#define TEMP_SENSOR_BED 1  // Bed NTC 100K
 
-// Define stepper settings here
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 500 }
+// Stepper motor configuration
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 500 }  // X, Y, Z, E (example values)
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 5, 25 }  // X, Y, Z, E
+#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 100, 10000 } // X, Y, Z, E
 
-// Uncomment to enable...
-//#define AUTO_BED_LEVELING_BILINEAR
-//#define AUTO_BED_LEVELING_3POINT
+// Temperature limits
+#define TEMP_HOTEND_MAX 250
+#define TEMP_BED_MAX 110
+
+// Acceleration settings
+#define DEFAULT_ACCELERATION          3000  // mm/s^2
+#define DEFAULT_RETRACT_ACCELERATION  3000  // mm/s^2
+
+// Jerk settings
+#define DEFAULT_XJERK 20.0  // mm/s
+#define DEFAULT_YJERK 20.0  // mm/s
+#define DEFAULT_ZJERK 0.4   // mm/s
+#define DEFAULT_EJERK 5.0    // mm/s
